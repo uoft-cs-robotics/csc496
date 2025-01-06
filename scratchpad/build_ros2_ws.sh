e256872cfce0a7d4049b61a341dfec7bcb9caf00
Radian Gondokaryono <radian.gondokaryono@mail.utoronto.ca>
2025-01-06 16:34:21 -0500

    Better submodules

--- scratchpad/build_ros2_ws.sh
+++ scratchpad/build_ros2_ws.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-cd slicer_ros2_module
+cd ros2_ws
 source /opt/ros/humble/setup.bash
 colcon build --cmake-args \
   -DSlicer_DIR:PATH=/home/csc379/slicer/Slicer-SuperBuild-Debug/Slicer-build \

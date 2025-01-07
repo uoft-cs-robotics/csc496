5412f6108124e7462982d8f1bb357dd9847897bd
Radian <radian.gondokaryono@mail.utoronto.ca>
2025-01-06 21:24:54 -0500

    no cache build, correct bashrc

--- docker/build_docker_container.sh
+++ docker/build_docker_container.sh
@@ -1,3 +1,3 @@
 docker-compose -f docker-compose-gui.yml down -v --remove-orphans
-docker-compose -f docker-compose-gui.yml build --no-cache
+docker-compose -f docker-compose-gui.yml build
 docker-compose -f docker-compose-gui.yml up --no-start
